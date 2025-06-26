0aa5cc54128e8b440bfc86db22893c628eacb655
Ionut Traistaru <ionut.m.traistaru@gmail.com>
2025-06-26 09:19:55 +0300

    refactor: remove redundant InitialiseConfigFile calls in setter methods and set default LLM provider to an empty string

--- internal/config/config.go
+++ internal/config/config.go
@@ -66,10 +66,6 @@ func (c *config) Storage() string {
 }
 
 func (m *config) SetEditor(editor string) error {
-	if _, err := InitialiseConfigFile(); err != nil {
-		return err
-	}
-
 	if editor == m.Editor() {
 		return nil
 	}
@@ -90,10 +86,6 @@ func (c *config) GetLLMProvider() (string, error) {
 }
 
 func (c *config) SetLLMProvider(provider string) error {
-	if _, err := InitialiseConfigFile(); err != nil {
-		return err
-	}
-
 	if provider == "" {
 		return fmt.Errorf("%s cannot be empty", LLMProviderKey)
 	}
@@ -124,10 +116,6 @@ func (c *config) GetLLMModel() (string, error) {
 }
 
 func (c *config) SetLLMModel(model string) error {
-	if _, err := InitialiseConfigFile(); err != nil {
-		return err
-	}
-
 	if model == "" {
 		return fmt.Errorf("%s cannot be empty", LLMModelKey)
 	}
@@ -181,10 +169,6 @@ func (c *config) GetVertexAILocation() (string, error) {
 }
 
 func (c *config) SetVertexAIProjectID(projectID string) error {
-	if _, err := InitialiseConfigFile(); err != nil {
-		return err
-	}
-
 	if projectID == "" {
 		return fmt.Errorf("%s cannot be empty", VertexAIProjectIDKey)
 	}
@@ -195,10 +179,6 @@ func (c *config) SetVertexAIProjectID(projectID string) error {
 }
 
 func (c *config) SetVertexAILocation(location string) error {
-	if _, err := InitialiseConfigFile(); err != nil {
-		return err
-	}
-
 	if location == "" {
 		return fmt.Errorf("%s cannot be empty", VertexAILocationKey)
 	}
@@ -249,6 +229,7 @@ func InitialiseConfigFile() (string, error) {
 
 		if _, err := os.Stat(configPath); os.IsNotExist(err) {
 			viper.SetDefault(EditorKey, GetEditor())
+			viper.SetDefault(LLMProviderKey, "")
 			viper.SetDefault(LLMApiKey, "")
 			viper.SetDefault(LLMModelKey, "")
 			viper.SetDefault(VertexAIProjectIDKey, "")
