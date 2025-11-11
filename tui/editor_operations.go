70a0ddd4cc5bc7667723654ca906724a82ec6db5
Ionut Traistaru <ionut.m.traistaru@gmail.com>
2025-11-11 22:12:45 +0000

    refactor(tui): clean up editor operations

--- tui/editor_operations.go
+++ tui/editor_operations.go
@@ -3,8 +3,8 @@ package tui
 import (
 	"strings"
 
-	"github.com/charmbracelet/lipgloss"
 	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
 	editor "github.com/ionut-t/goeditor/adapter-bubbletea"
 )
 
@@ -20,9 +20,7 @@ func (m *model) resetEditor() tea.Cmd {
 func (m *model) applyQueryToEditor(query string) tea.Cmd {
 	m.editor.SetContent(query)
 	m.editor.Focus()
-	if err := m.editor.SetCursorPositionEnd(); err != nil {
-		// Cursor positioning failed, but continue anyway
-	}
+	_ = m.editor.SetCursorPositionEnd()
 	m.view = viewMain
 	m.focused = focusedEditor
 	ed, cmd := m.editor.Update(nil)
@@ -34,12 +32,6 @@ func (m *model) applyQueryToEditor(query string) tea.Cmd {
 func (m *model) focusEditor() {
 	m.focused = focusedEditor
 	m.editor.Focus()
-}
-
-// focusContent changes focus to the content component
-func (m *model) focusContent() {
-	m.focused = focusedContent
-	m.editor.Blur()
 }
 
 // finishQueryExecution sets common state after query execution
